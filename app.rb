13417745f4ec7fa1c030d61322b61125c1802b75
S-H-GAMELINKS <gamelinks007@gmail.com>
2020-01-10 18:22:45 +0900

    add Header

--- app.rb
+++ app.rb
@@ -7,9 +7,27 @@ class MyApp < Ovto::App
   class Actions < Ovto::Actions
   end
 
+  class Header < Ovto::Component
+    def render
+        o 'nav' do
+            o 'div.nav-wrapper' do
+                o 'a.brand-logo', 'Hamada Onsen Map'
+                o 'ul#nav-mobile.right hide-on-med-and-down' do
+                    o 'li' do
+                        o 'a', {href: 'https://github.com/Hamada-rb/onsen'}, 'GitHub'
+                    end
+                end
+            end
+        end
+    end
+  end
+
   class MainComponent < Ovto::Component
     def render
-        o 'h1', 'Hamada Onsen Map'
+        o 'div' do
+            o Header
+            o 'h1', 'Hamada Onsen Map'
+        end
     end
   end
 end
