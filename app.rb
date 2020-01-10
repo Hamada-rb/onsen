b45e20cbc900fa43f8438c7cddc0a6b6555240df
S-H-GAMELINKS <gamelinks007@gmail.com>
2020-01-10 23:52:29 +0900

    impl search

--- app.rb
+++ app.rb
@@ -3,21 +3,40 @@ require 'json'
 
 class MyApp < Ovto::App
   class State < Ovto::State
+    item :word, default: ""
     item :onsen, default: [
       {
         name: "旭温泉あさひ荘",
         address: "〒697-0427 島根県浜田市旭町木田９５４−３",
-        url: "http://asahionsen.net/selected/"   
+        url: "http://asahionsen.net/selected/",
+        showable: true   
       },
       {
         name: "美又温泉会館",
         address: "697-0301 島根県浜田市金城町追原11番乙地",
-        url: "http://www.kankou-hamada.org/modules/guide/index.php?action=SpotView&spot_id=1043"
+        url: "http://www.kankou-hamada.org/modules/guide/index.php?action=SpotView&spot_id=1043",
+        showable: true
       }
     ]
   end
 
   class Actions < Ovto::Actions
+    def set_word(word:)
+      { word: word }
+    end
+
+    def search(word:)
+      result = state.onsen.map{|onsen|
+        o = onsen
+        if o["name"].include?(word)
+          o["showable"] = true
+        else
+          o["showable"] = false
+        end
+        o
+      }
+      { onsen: result }
+    end
   end
 
   class Header < Ovto::Component
@@ -39,13 +58,15 @@ class MyApp < Ovto::App
     def render(onsens:)
       o 'div.row' do
         onsens.each do |onsen|
-          o 'div.col', style: {width: '100%'} do
-            o 'div.card' do
-              o 'div.card-content.white-text' do
-                o 'span.card-title', onsen["name"]
-                o 'p', onsen["address"]
-                o 'div.card-action' do
-                  o 'a', { href: onsen["url"] }, "Webサイトへ"
+          if onsen["showable"]
+            o 'div.col', style: {width: '100%'} do
+              o 'div.card' do
+                o 'div.card-content.white-text' do
+                  o 'span.card-title', onsen["name"]
+                  o 'p', onsen["address"]
+                  o 'div.card-action' do
+                    o 'a', { href: onsen["url"] }, "Webサイトへ"
+                  end
                 end
               end
             end
@@ -61,6 +82,12 @@ class MyApp < Ovto::App
             o Header
             o 'div.container' do
                 o 'h1.center-align', 'Hamada Onsen Map'
+                o 'input', { type: 'text', onchange: ->(e){ actions.set_word(word: e.target.value) }, value: state.word}
+                o 'span.center-align' do
+                  o 'div.link-btn' do
+                    o 'a.btn', { onclick: ->(e){ actions.search(word: state.word)}}, "search"
+                  end
+                end
                 o Onsens, onsens: state.onsen
             end
         end
