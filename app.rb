4cc254f329553e74585c8a0dc12eabde30eef4d3
S-H-GAMELINKS <gamelinks007@gmail.com>
2020-01-10 22:23:37 +0900

    Add Onsen components

--- app.rb
+++ app.rb
@@ -1,7 +1,16 @@
 require 'ovto'
+require 'json'
 
 class MyApp < Ovto::App
   class State < Ovto::State
+    item :onsen, default: [
+      {
+          name: "旭温泉あさひ荘",
+          address: "〒697-0427 島根県浜田市旭町木田９５４−３",
+          about: "【営業時間】9時～20時／【定休日】毎週水曜日【その他ご案内】当施設は日帰りのみです※宿泊はできません",
+          url: "http://asahionsen.net/selected/"   
+      }
+    ]
   end
 
   class Actions < Ovto::Actions
@@ -22,12 +31,34 @@ class MyApp < Ovto::App
     end
   end
 
+  class Onsens < Ovto::Component
+    def render(onsens:)
+      o 'div.row' do
+        onsens.each do |onsen|
+          o 'div.col', style: {width: '100%'} do
+            o 'div.card' do
+              o 'div.card-content.white-text' do
+                o 'span.card-title', onsen["name"]
+                o 'p', onsen["address"]
+                o 'p', onsen["about"]
+                o 'div.card-action' do
+                  o 'a', { href: onsen["url"] }, "Webサイトへ"
+                end
+              end
+            end
+          end
+        end
+      end
+    end
+  end
+
   class MainComponent < Ovto::Component
     def render
         o 'div' do
             o Header
             o 'div.container' do
                 o 'h1', 'Hamada Onsen Map'
+                o Onsens, onsens: state.onsen
             end
         end
     end
